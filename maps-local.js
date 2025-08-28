e67b612ca136600345da76d1d04da0ad4c0c8755
Fred Meng <fredmeng@fm2.local>
2025-08-28 14:32:38 +1000

    update

--- maps-local.js
+++ maps-local.js
@@ -1,2 +1,7 @@
-// use the map
-drawLinesSequentially(coords, 700);
+// rendering
+window.onload = function() {
+  // Delay line rendering slightly to ensure markers are visible
+  setTimeout(() => {
+    drawLinesSequentially(coords, 700); // or use addPolylineToMap()
+  }, 1000); // 1-second delay after full load
+};
