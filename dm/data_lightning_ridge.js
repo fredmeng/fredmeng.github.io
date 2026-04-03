36e7f3509e076251f6979e20b4d819672034833d
Fred Meng <fredmeng@fm2.local>
2026-04-04 10:54:21 +1100

    update

---
+++ dm/data_lightning_ridge.js
@@ -0,0 +1,27 @@
+const meta = {
+  title: "Road Trip: Sydney to Lightning Ridge (2024)",
+  description: "Lightning Ridge: Discover the Black Opal Capital of Australia"
+};
+
+const coords = [
+  [-33.8804, 151.1155, "Croydon 克羅伊登"],
+  [-32.8167, 151.4833, "Kurri Kurri 庫里庫里"],
+  [-32.5628, 151.1739, "Singleton 辛格爾頓"],
+  [-32.2653, 150.8878, "Muswellbrook 馬斯威爾布魯克"],
+  [-32.0500, 150.8667, "Scone 斯康"],
+  [-30.9833, 150.2500, "Gunnedah 根尼達"],
+  [-30.3333, 149.7833, "Narrabri 納拉布賴"],
+  [-30.2167, 149.4333, "Wee Waa 威瓦"],
+  [-30.0167, 148.1167, "Walgett 沃爾格特"],
+  [-29.4261, 147.9786, "Lightning Ridge 閃電嶺"],
+  [-30.94718, 148.27155, "Coonamble 庫納姆布林"],
+  [-31.3333, 148.5333, "Gulargambone 古拉甘博恩"],
+  [-31.28183, 148.48544, "Gilgandra 吉爾甘德拉"],
+  [-32.0167, 149.4000, "Dunedoo 丹尼杜"],
+  [-32.5900, 149.5800, "Mudgee 馬奇"],
+  [-32.8500, 149.9667, "Rylstone 萊爾斯通"],
+  [-32.8500, 150.0000, "Kandos 坎多斯"],
+  [-33.4833, 150.1500, "Lithgow 利斯戈"],
+  [-33.7125, 150.3119, "Katoomba 卡通巴"],
+  [-33.8804, 151.1155, "Croydon 克羅伊登"]
+];
