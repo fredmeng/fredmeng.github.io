21d30c852fafd961b5cc9abc7d3af23b92974780
Fred Meng <fredmeng@fm2.local>
2026-04-04 10:58:41 +1100

    update

---
+++ dm/data_tasmania.js
@@ -0,0 +1,29 @@
+const meta = {
+  title: "Road Trip: Tasmania (2025)",
+  description: "Embark on the ultimate Tasmanian road trip."
+};
+
+
+const coords = [
+[-41.4333, 147.1167, "Launceston 朗塞斯頓"],
+  [-41.3833, 146.3333, "Sheffield 謝菲爾德"],
+  [-41.1167, 146.0667, "Penguin 企鵝鎮"],
+  [-40.9833, 145.7167, "Wynyard 溫亞德"],
+  [-40.7667, 145.3000, "Stanley 史丹利"],
+  [-40.8500, 145.1167, "Smithton 史密斯頓"],
+  [-40.9333, 144.6833, "Marrawah 瑪拉瓦"],
+  [-41.0500, 144.6667, "Arthur River 亞瑟河"],
+  [-40.7667, 145.3000, "Stanley 史丹利"],
+  [-41.6833, 145.9167, "Cradle Mountain 搖籃山"],
+  [-42.0833, 145.5500, "Queenstown 皇后鎮"],
+  [-42.1333, 146.2167, "Derwent Bridge 德文特橋"],
+  [-42.7833, 147.0667, "New Norfolk 新諾福克"],
+  [-42.8821, 147.3272, "Hobart 荷巴特"],
+  [-42.7333, 147.4333, "Richmond 里奇蒙"],
+  [-42.1167, 148.2833, "Coles Bay 寇斯灣"],
+  [-41.3333, 148.2500, "St Helens 聖海倫斯"],
+  [-41.2500, 148.2833, "Binalong Bay 賓納隆灣"],
+  [-41.2917, 147.9167, "Pub in the Paddock 圍場酒吧"],
+  [-41.2167, 147.8000, "Derby 德比"],
+  [-41.4333, 147.1167, "Launceston 朗塞斯頓"]
+];
