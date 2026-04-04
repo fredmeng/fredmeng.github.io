81435ea2e2526ca15843cf6210aa6d7c17d256d6
Fred Meng <fredmeng@fm2.local>
2026-04-04 11:39:57 +1100

    update

---
+++ dm/data_armidale.js
@@ -0,0 +1,22 @@
+const meta = {
+  title: "Road Trip: Sydney to Armidale (2025)",
+  description: "A drive through Australia's highest city - Armidale."
+};
+
+const coords = [
+  [-33.8804, 151.1155, "Croydon 克羅伊登"],
+  [-32.6333, 151.6167, "Paterson 帕特森"],
+  [-32.5333, 151.5167, "Vacy 瓦西"],
+  [-32.4833, 151.7167, "Wirragulla 威拉古拉"],
+  [-32.4500, 151.7500, "Dungog 丹戈格"],
+  [-32.4333, 151.5167, "East Gresford 東格雷斯福德"],
+  [-32.5628, 151.1739, "Singleton 辛格爾頓"],
+  [-31.6417, 150.7167, "Willow Tree 柳樹鎮"],
+  [-31.0833, 150.9167, "Tamworth 塔姆沃思"],
+  [-30.5167, 151.6667, "Armidale 阿米代爾"],
+  [-30.6500, 151.5000, "Uralla 尤拉拉"],
+  [-30.9833, 151.6000, "Walcha 沃爾恰"],
+  [-32.3833, 150.6833, "Denman 丹曼"],
+  [-32.9333, 151.1500, "Wollombi 沃隆比"],
+  [-33.8804, 151.1155, "Croydon 克羅伊登"]
+];
---
+++ dm/data_nsw_south_coast.js
@@ -0,0 +1,22 @@
+const meta = {
+  title: "Road Trip: Sydney to  NSW's South Coast (2023)",
+  description: "A drive through NSW's South Coast."
+};
+
+const coords = [
+  [-33.8804, 151.1155, "Croydon 克羅伊登"],
+  [-34.7744, 150.6972, "Berry 貝里"],
+  [-34.8722, 150.6033, "Nowra 諾拉"],
+  [-35.7081, 150.1744, "Batemans Bay 貝特曼斯灣"],
+  [-36.0911, 150.0594, "Bodalla 博達拉"],
+  [-36.2167, 150.1333, "Narooma 納魯馬"],
+  [-36.3294, 150.0786, "Central Tilba 中央蒂爾巴"],
+  [-36.6711, 149.8417, "Bega 貝加"],
+  [-36.7333, 149.9833, "Tothra 塔斯拉"],
+  [-36.3667, 150.0667, "Wallaga Lake 瓦拉加湖"],
+  [-35.9108, 150.0825, "Moruya 莫魯亞"],
+  [-35.3833, 150.4500, "Burrill Lake 伯里爾湖"],
+  [-35.3561, 150.4731, "Ulladulla 烏拉杜拉"],
+  [-35.3214, 150.4361, "Milton 米爾頓"],
+  [-33.8804, 151.1155, "Croydon 克羅伊登"]
+];
